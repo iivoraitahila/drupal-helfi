a652d19915e90cdb4548039e0f7ac1382330e452
Iivo Raitahila <iivo.raitahila@ibm.com>
2021-04-27 15:33:18 +0300

    hello world

--- docker/openshift/docker-entrypoint.sh
+++ docker/openshift/docker-entrypoint.sh
@@ -3,10 +3,10 @@
 echo "Copy Drupal to the root folder of the web server"
 # copy files to Azure Disk after the container has started and volumes have been mounted
 rsync -vah "/opt/drupal/public/" "/var/www/html/public/" --exclude="web/sites/default/files" --delete
-rsync -vah "/opt/drupal/vendor/" "/var/www/html/vendor/" --delete
-rsync -vah "/opt/drupal/conf/" "/var/www/html/conf/" --delete
-rsync -vah "/opt/drupal/drush/" "/var/www/html/drush/" --delete
-cp /opt/drupal/composer.* /var/www/html/
+#rsync -vah "/opt/drupal/vendor/" "/var/www/html/vendor/" --delete
+#rsync -vah "/opt/drupal/conf/" "/var/www/html/conf/" --delete
+#rsync -vah "/opt/drupal/drush/" "/var/www/html/drush/" --delete
+#cp /opt/drupal/composer.* /var/www/html/
 # copy /opt/drupal/drush (and other files & folders) to /var/www/html as they become necessary
 
 # mount permanent storage (public_files)
@@ -21,7 +21,7 @@ fi
 echo "To install Drupal for the first time (clears database), execute"
 echo "drush site:install --existing-config"
 echo "in /var/www/html/public via the terminal"
-cd /var/www/html/public && drush cr && drush cim -y
+#cd /var/www/html/public && drush cr && drush cim -y
 
 echo "Run PHP-FPM in the background as a daemon"
 php-fpm
